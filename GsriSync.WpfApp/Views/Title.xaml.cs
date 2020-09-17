b758ae2c565c711996ca95ca00dda1f284d0346c
Arwyn <10365998+ArwynFr@users.noreply.github.com>
2020-09-17 22:42:40 +0200

    Review branding

---
+++ GsriSync.WpfApp/Views/Title.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace GsriSync.WpfApp.Views
+{
+    /// <summary>
+    /// Logique d'interaction pour Title.xaml
+    /// </summary>
+    public partial class Title : UserControl
+    {
+        public Title()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+    }
+}
