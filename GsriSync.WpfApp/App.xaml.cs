b36e32c551d549fed36b19725b30f7a13de384fa
Arwyn <10365998+ArwynFr@users.noreply.github.com>
2020-09-19 18:34:22 +0200

    Setup Autofac

--- GsriSync.WpfApp/App.xaml.cs
+++ GsriSync.WpfApp/App.xaml.cs
@@ -1,9 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Configuration;
-using System.Data;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
 using System.Windows;
 
 namespace GsriSync.WpfApp
@@ -13,5 +9,24 @@ namespace GsriSync.WpfApp
     /// </summary>
     public partial class App : Application
     {
+        private readonly IServiceProvider _serviceProvider;
+
+        public App()
+        {
+            var collection = new ServiceCollection();
+            ConfigureServices(collection);
+            _serviceProvider = collection.BuildServiceProvider();
+        }
+
+        private void Application_Startup(object sender, StartupEventArgs e)
+        {
+            var mainWindow = _serviceProvider.GetService<MainWindow>();
+            mainWindow.Show();
+        }
+
+        private void ConfigureServices(ServiceCollection services)
+        {
+            services.AddSingleton<MainWindow>();
+        }
     }
 }
